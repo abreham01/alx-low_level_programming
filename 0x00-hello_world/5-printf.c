1b54e2dc662318c65f5d8dc48a4d2bea362c59dd
abreham01 <abrehamtesfaye200@gmail.com>
2023-06-15 03:22:29 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - prints out a string string s followed by a new line using printf
+* Return: int 0 when done and no errors
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
