7216b04928fb1162c795ab0ee0ecd1ff9f0b8564
abreham01 <abrehamtesfaye200@gmail.com>
2023-06-16 04:52:59 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+* main - Prints all numbers of base 10
+* Return: 0
+**/
+
+int main (void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		printf("%i", num);
+		num++;
+	}
+	
+	printf("\n");
+
+	return (0);
+}
