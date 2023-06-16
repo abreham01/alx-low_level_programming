0fc2399f5729a415ae54977ced309f60ebce4ba5
abreham01 <abrehamtesfaye200@gmail.com>
2023-06-16 06:36:23 -0700

    task 10 fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,12 +17,12 @@ int main(void)
 			{
 				putchar(num1);
 				putchar(num2);
-			}
 
-			if (!(num1 == 56 && num2 == 57))
-			{
-				putchar(',');
-				putchar(' ');
+				if (!(num1 == 56 && num2 == 57))
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
