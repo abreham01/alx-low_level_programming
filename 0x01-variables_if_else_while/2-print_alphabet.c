e0e120fba2ba2ee022ba4f17a2f5de7a93fb29dc
abreham01 <abrehamtesfaye200@gmail.com>
2023-06-16 04:14:12 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+/**
+* main - Prints all alphabets in lowercase
+* Return: 0
+**/
+
+int main(void)
+{
+	for (l_letter = 97; l_letter <= 122; l_letter++)
+	{
+		putchar(l_letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
