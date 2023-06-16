c5821f853b27fd36d59bafa739df90d5b4792075
abreham01 <abrehamtesfaye200@gmail.com>
2023-06-16 05:28:22 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+* main - Prints all lowercase alphabets in reverse order using putchar
+* Return: 0
+**/
+
+int main(void)
+{
+	char letter = 122;
+
+	while (letter >= 97)
+	{
+		putchar(letter);
+		num++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
