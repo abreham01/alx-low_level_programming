f268a61917f25f6d5187beed8f23303de76dcc72
abreham01 <abrehamtesfaye200@gmail.com>
2023-06-16 07:31:07 -0700

    task 12 fix 1

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -20,16 +20,16 @@ int main(void)
 				putchar(' ');
 				putchar ((b / 10) + '0');
 				putchar ((b % 10) + '0');
-				
+
 				if (!(a == 98 && b == 99))
 				{
 					putchar(',');
 					putchar(' ');
 				}
-			}			
+			}
 		}
 	}
-	
+
 	putchar ('\n');
 	return (0);
 }
