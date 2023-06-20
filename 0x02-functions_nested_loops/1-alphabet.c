c35c3bef22db493a15b31af66b2fb87219d9bec6
abreham01 <abrehamtesfaye200@gmail.com>
2023-06-20 09:31:39 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - Print alphabet in lowercase using only `_putchar`
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char letters = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(letters);
+		letters++;
+	}
+	_putchar('\n');
+}
