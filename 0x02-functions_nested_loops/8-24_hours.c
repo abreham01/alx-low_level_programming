c747c96c3ccd4a62e4aba81fbab0ef5a604b8856
abreham01 <abrehamtesfaye200@gmail.com>
2023-06-21 02:10:08 -0700

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * jack_bauer - Print every minute of the day of Jack Bauer
+ * Description: Start from 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+	int hour;
+	int minute;
+
+	for (hour = 0; hour < 23; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			if (hour <10 )
+			{
+				_putchar('0');
+			}
+
+			_putchar(hour + '0');
+			_putchar(':');
+
+			if (minute <10 )
+                        {
+                                _putchar('0');
+                        }
+			_putchar(hour + '0');
+
+	}
+}
