eb99f88d5b3ef7e24f35e4672edd27d1627128af
abreham01 <abrehamtesfaye200@gmail.com>
2023-06-22 03:34:58 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_most_numbers - prints 0-9 excep 2 and 4 with a new line
+*
+* Return: void. no return.
+*/
+
+void print_most_numbers(void);
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (!(i == 2 && i == 4))
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
+}
