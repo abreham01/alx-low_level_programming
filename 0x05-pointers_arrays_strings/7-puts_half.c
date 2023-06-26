fb57a39786be5573e60e0e649e4d847f2da01597
abreham01 <abrehamtesfaye200@gmail.com>
2023-06-26 06:56:16 -0700

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: char to check
+ *
+ * Return: 0 is success
+ */
+void puts_half(char *str)
+{
+	int index = 0;
+	int n;
+
+	while (str[index] != '\0')
+		{
+			index++;
+		}
+
+	if (index % 2 = 0)
+	{
+		n = (index - 1) / 2;
+	}
+
+	else
+	{
+		n = (index / 2);
+	}
+
+	n++;
+
+	for (index = n; str[index] != '\0'; index++)
+	{
+		_putchar(str[index]);
+	}
+
+	_putchar('\n');
+}
