8605a641dc3aa905f93ed2c0e9408cd7c8d600e6
abreham01 <abrehamtesfaye200@gmail.com>
2023-06-26 06:23:23 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: char to check
+ *
+ * Return: 0 is success
+ */
+
+void puts2(char *str)
+{
+	int index;
+
+	for (index = 0; str[index] != '\0'; index += 2)
+	{
+		_putchar(str[index]);
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
+
