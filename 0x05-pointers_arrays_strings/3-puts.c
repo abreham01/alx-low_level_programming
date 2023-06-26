a2c63f588e6446e02df7da497514b940919698f5
abreham01 <abrehamtesfaye200@gmail.com>
2023-06-26 02:41:57 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by a new line
+ * @str: string type
+ *
+ * Return: 0 is success
+ */
+void _puts(char *str)
+{
+	int index;
+
+	for (index = 0; *(str + index) != '\0'; index++)
+	{
+		_putchar(*(str + index));
+	}
+
+	_putchar('\n');
+}
