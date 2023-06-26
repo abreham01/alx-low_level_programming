b7fcfc57c5f9a83b212693d3c94011204f2db837
abreham01 <abrehamtesfaye200@gmail.com>
2023-06-26 03:38:52 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in a reverse order.
+ * @s: string type
+ *
+ * Return: 0 is success
+ */
+void print_rev(char *s)
+{
+	int index;
+
+	while (s[index] != '\0')
+	{
+		index++;
+        }
+
+	for (index--; index >= 0; index--)
+	{
+		_putchar(str[index]);
+	}
+
+	_putchar('\n');
+}
