0cbf2cf7079710f1442d7459668a6fe753903d9c
abreham01 <abrehamtesfaye200@gmail.com>
2023-07-03 06:41:02 -0700

    task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory
+ * @dest: char
+ * @src: char 
+ * @n: unsigned int
+ *
+ * Return: 0 is success
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	while (a < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
