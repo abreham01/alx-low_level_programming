5fa51c6bbbc8a3ec21178bd8e6c1891ea20ada59
abreham01 <abrehamtesfaye200@gmail.com>
2023-07-05 13:04:21 -0700

    task 0

---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+this folder contains files fir the project 0x08-recursion
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+
+#endif
