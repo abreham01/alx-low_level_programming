ce6594b4d0a1d65658bfe66e690e8e9cec71e873
abreham01 <abrehamtesfaye200@gmail.com>
2023-07-07 06:08:38 -0700

    task 0 fix 2

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,9 +8,14 @@
  * Return: Always o.
  */
 
-int main(int __attribute__(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[0]);
+	}
 
 	return (0);
 }
