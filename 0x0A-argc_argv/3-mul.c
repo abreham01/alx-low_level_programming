56066d670a37ce4a965ad4d5c6c59b46364b9bc2
abreham01 <abrehamtesfaye200@gmail.com>
2023-07-07 09:17:01 -0700

    task 3 fix

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,12 +11,12 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc < 2)
+	if (argc != 3)
 	{
 		printf("Error");
 		return (1);
 	}
-	
+
 	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 
 	return (0);
