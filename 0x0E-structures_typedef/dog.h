ed24ee3dac4445d52cfb6ae8be36cc31eec4d577
Abreham <abrehamtesfaye200@gmail.com>
2023-07-17 18:33:47 +0300

    task 0 fix 2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,23 +1,27 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef _DOG_H
+#define _DOG_H
 
 /**
- * struct dog - Dog struct
- * @name: The name of the dog.
- * @age: The age of the dog.
- * @owner: The owner of the dog.
+ * struct dog - a new type
+ *
+ * @name: the name of the dog (Django)
+ * @age: the age of the dog (3.5)
+ * @owner: the name of the owner (Jay)
  */
-
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog;
+}
+	dog;
+/*task three, new type dog_t as new name of strcut dog */
+typedef struct dog dog_t;
 
+int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif
+#endif /* DOG */
