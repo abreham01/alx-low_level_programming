24b9777c78f9975fc6cc11d9290636388225eafc
abreham01 <abrehamtesfaye200@gmail.com>
2023-07-20 02:21:58 -0700

    task 0 fix 2

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,14 +14,15 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 
 	va_list args;
+
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(args, int);
 	}
-	
+
 	va_end(args);
-	
+
 	return (sum);
 }
