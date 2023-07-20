d91dfe178062fe775d0d361d4700228cde4df9eb
abreham01 <abrehamtesfaye200@gmail.com>
2023-07-20 02:55:59 -0700

    task 3 fix

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,93 +1,51 @@
+#include <stdio.h>
+#include <stdarg.h>
 #include "variadic_functions.h"
 
 /**
- * print_all - prints anything passed as an argument
- *
- * @format: list of types of arguments passed to function
- */
-
+* print_all - print char, integer, float and string
+* @format: format
+*/
 void print_all(const char * const format, ...)
 {
-	va_list args;
-	int i = 0;
-	int j;
+	va_list list;
+	unsigned int j = 0, start = 0;
+	char *p;
 
-	char *sep = "";
-
-	p_forms pf_fmt[] = {
-		{"c", (*print_char)},
-		{"i", (*print_int)},
-		{"f", (*print_float)},
-		{"s", (*print_string)},
-		{NULL, NULL}
-	};
-
-	va_start(args, format);
-
-	while (format && format[i])
+	va_start(list, format);
+	while (format && format[j] != '\0')
 	{
-		j = 0;
-		while (pf_fmt[j].f_sym)
-		{
-			if (format[i] == *(pf_fmt[j].f_sym))
-			{
-				printf("%s", sep);
-				pf_fmt[j].p_func(args);
-				sep = ", ";
-			}
-			j++;
-		}
-		i++;
+		switch (format[j])
+		{ case 'c':
+			switch (start)
+			{ case 1: printf(", "); }
+			start = 1;
+			printf("%c", va_arg(list, int));
+			break;
+			case 'i':
+			switch (start)
+			{ case 1: printf(", "); }
+			start = 1;
+			printf("%i", va_arg(list, int));
+			break;
+		case 'f':
+			switch (start)
+			{ case 1: printf(", "); }
+			start = 1;
+			printf("%f", va_arg(list, double));
+			break;
+		case's':
+			switch (start)
+			{ case 1: printf(", "); }
+			start = 1;
+			p = va_arg(list, char*);
+			if (p)
+			{ printf("%s", p);
+			break; }
+			printf("%p", p);
+			break; }
+		j++;
 	}
 	printf("\n");
-	va_end(args);
-}
-
-/**
- * print_char - prints char passed as argument to variable list
- *
- * @args: variable args list passed from print_all
- */
-
-void print_char(va_list args)
-{
-	printf("%c", va_arg(args, int));
-}
-
-/**
- * print_int - prints integer passed as an argument to variable list
- *
- * @args: variable args list passed from print_all
- */
-
-void print_int(va_list args)
-{
-	printf("%d", va_arg(args, int));
-}
-
-/**
- * print_float - prints floating point passed as an argument to variable list
- *
- * @args: variable args list passed from print_all
- */
-
-void print_float(va_list args)
-{
-	printf("%f", va_arg(args, double));
-}
-
-/**
- * print_string - prints string passed as an argument to variable list
- *
- * @args: variable args list passed from print_all
- */
-
-void print_string(va_list args)
-{
-	char *s = va_arg(args, char *);
-
-	if (s == NULL)
-		s = "(nil)";
-
-	printf("%s", s);
+	va_end(list);
 }
