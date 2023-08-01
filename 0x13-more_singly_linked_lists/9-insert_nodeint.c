cbcafaaa5f9bccaed3285a5d2d670a7f6fbc65c6
Abreham <abrehamtesfaye200@gmail.com>
2023-08-01 17:29:22 +0300

    task 9 fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,7 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 		while (i < idx - 1 && ptr != NULL)
 		{
-			temp = temp->next;
+			ptr = ptr->next;
 			i++;
 		}
 
